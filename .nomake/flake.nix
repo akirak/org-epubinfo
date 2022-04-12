a7bbcb9a54404efb3c68e12cdaa1e85f9b995ef0
Akira Komamura <akira.komamura@gmail.com>
2022-04-12 11:49:12 +0900

    chore: Add nomake

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    org-epubinfo = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-epubinfo";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
